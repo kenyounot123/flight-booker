09bafb9f7da268049e0b9d683441f80770d11374
kenyounot123 <kenlu519@gmail.com>
2023-11-30 21:28:20 -0500

    Finish #addPassengerField in stimulus controller

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -6,10 +6,18 @@ export default class extends Controller {
   /* Get time in milliseconds and replace child_index with that time
   This allows for a unique index for each dynamically added nested form */
   addPassengerField() {
+    // Initializes a new Date object and get the time in milliseconds.
     const date_time_index = new Date().getTime()
-    const uniqueChildIndex = date_time_index
-    var content = this.passengerFormTarget.innerHTML.replace(/NEW_RECORD/g, uniqueChildIndex)
-    const formClone = content.cloneNode(true)
+    // Creates a copy of the template's content along with all its child nodes.
+    // this returns a DocumentFragment object.
+    const formClone = this.passengerFormTarget.content.cloneNode(true)
+    // This selects the form element of the cloned DocumentFragment object 
+    const clonedContent = formClone.querySelector('form')
+    // Replaces the cloned content's innerHTML instead of the template one so that we can reuse the template
+    const modifiedContent = clonedContent.innerHTML.replace(/NEW_RECORD/g, date_time_index)
+    // Update the cloned content's innerHTML with the modified content.
+    clonedContent.innerHTML = modifiedContent
+    // formClone is now updated and ready to be appended to our target
     this.newPassengerFormContainerTarget.appendChild(formClone)
   }
 
