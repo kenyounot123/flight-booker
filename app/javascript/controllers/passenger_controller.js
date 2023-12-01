8534d9d3f7b7b36acd1c2a480bbb3154bdee7827
kenyounot123 <kenlu519@gmail.com>
2023-11-30 20:01:37 -0500

    Add child_index attribute to nested form

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -1,11 +1,19 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ["passengerForm", "newPassengerContainer"]
+  static targets = ["passengerForm", "newPassengerFormContainer"]
 
+  /* Get time in milliseconds and replace child_index with that time
+  This allows for a unique index for each dynamically added nested form */
   addPassengerField() {
-    const date = new Date()
-    const formClone = this.passengerFormTarget.content.cloneNode(true)
-    this.newPassengerContainerTarget.appendChild(formClone)
+    const date_time_index = new Date().getTime()
+    const uniqueChildIndex = date_time_index
+    var content = this.passengerFormTarget.innerHTML.replace(/NEW_RECORD/g, uniqueChildIndex)
+    const formClone = content.cloneNode(true)
+    this.newPassengerFormContainerTarget.appendChild(formClone)
+  }
+
+  deletePassengerField() {
+
   }
 }
