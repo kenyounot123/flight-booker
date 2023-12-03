0dc5db61778a1d35e8f266a2eb3bb7d33bbc461b
kenyounot123 <kenlu519@gmail.com>
2023-12-03 16:11:48 -0500

    Update PassengerMailer confirmation_email action

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,5 +1,10 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notifications@example.com'
 
   def confirmation_email
+    @booking = params[:booking]
+    @url = booking_url(@booking)
+    mail to: @booking.passengers.pluck(:email),
+         subject: "Confirmation email for #{@passenger.email}"
   end
 end
--- test/mailers/previews/passenger_mailer_preview.rb
+++ test/mailers/previews/passenger_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/passenger_mailer
 class PassengerMailerPreview < ActionMailer::Preview
-
+  def confirmation_email
+    booking = Booking.last
+    booking.passengers.each do |passenger|
+      PassengerMailer.with(booking: booking).confirmation_email(passenger)
+    end
+  end
 end
